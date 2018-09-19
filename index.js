a8e889b0c634da2a36b248c9e9480ed965fce135
Veronica Bustabad <vbustabad@gmail.com>
2018-09-19 03:32:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-function loadIssue() {
+function loadIssues() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  for(var i=0;i<issues.length;i++) {
-    var result = template(issues[i]);
-    document.getElementsByTagName("main")[0].innerHTML += result;
-  }
+  var result = template(issues);
+  document.getElementsByTagName("main")[0].innerHTML += result;
 }
